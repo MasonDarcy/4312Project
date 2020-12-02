a74812b836ec7980573397bfa08cbd7e87de2544
Mason D'Arcy <masondarcy@gmail.com>
2020-12-02 17:49:21 -0500

    Add files via upload
    added additional data classes

---
+++ code/video-rental-project/src/main/java/data_objects/Movie.java
@@ -0,0 +1,53 @@
+package data_objects;
+
+public class Movie {
+	
+public Movie(int videoID, int year, String title, String category, String introduction, String directors, String producers, int stock) {
+	this.videoID = videoID;
+	this.year = year;
+	this.title = title;
+	this.category = category;
+	this.introduction = introduction;
+	this.directors = directors;
+	this.producers = producers;
+	this.stock = stock;
+}
+
+
+
+public int getVideoID() {
+	return videoID;
+}
+public int getYear() {
+	return year;
+}
+public String getTitle() {
+	return title;
+}
+public String getCategory() {
+	return category;
+}
+public String getIntroduction() {
+	return introduction;
+}
+public String getDirectors() {
+	return directors;
+}
+public String getProducers() {
+	return producers;
+}
+public int getStock() {
+	return stock;
+}
+
+
+
+private int videoID;
+private int year;
+private String title;
+private String category;
+private String introduction;
+private String directors;
+private String producers;
+private int stock;
+}
