983e3998d3959dd722a11d549014f5e65ab57718
Mason D'Arcy <masondarcy@gmail.com>
2020-12-04 06:37:28 -0500

    Update CustomerOrder.java

--- code/video-rental-project/src/main/java/data_objects/CustomerOrder.java
+++ code/video-rental-project/src/main/java/data_objects/CustomerOrder.java
@@ -2,4 +2,46 @@ package data_objects;
 
 public class CustomerOrder {
 
+	
+public CustomerOrder(int orderID, String dateOrdered, String shippingDestination, int period, String status,
+			String returnTrackingInformation, String email) {
+		this.orderID = orderID;
+		this.dateOrdered = dateOrdered;
+		this.shippingDestination = shippingDestination;
+		this.period = period;
+		this.status = status;
+		this.returnTrackingInformation = returnTrackingInformation;
+		this.email = email;
+	}
+private int orderID;
+private String dateOrdered;
+private String shippingDestination;
+private int period;
+private String status;
+private String returnTrackingInformation;
+private String email;
+
+public int getOrderID() {
+	return orderID;
+}
+public String getDateOrdered() {
+	return dateOrdered;
+}
+public String getShippingDestination() {
+	return shippingDestination;
+}
+public int getPeriod() {
+	return period;
+}
+public String getStatus() {
+	return status;
+}
+public String getReturnTrackingInformation() {
+	return returnTrackingInformation;
+}
+public String getEmail() {
+	return email;
+}
+
+
 }
